4d67df67c7bae669778260442f12bc82d4070577
Avcm00 <cuevaariel009@outlook.com>
2024-10-28 14:31:59 -0500

    se agrega ee codigo del nodo

--- Proyectodjango/principalticket/models.py
+++ Proyectodjango/principalticket/models.py
@@ -36,3 +36,60 @@ class LinkedList:
         else:
             print('None')
 
+
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+
+# material para el proyecto
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    #
+    def appendend(self, new_data):
+        new_node = Node(new_data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last = self.head
+        while last.next:
+            last = last.next
+        last.next = new_node
+
+    def appendstart(self, data):
+        new_node = Node(data)
+        if self.head:
+            new_node.next = self.head
+        self.head = new_node
+
+    def show(self):
+        current_node = self.head
+        if self.head:
+            while current_node:
+                print(current_node.data, end=' <- ')
+                current_node = current_node.next
+            print('new')
+        else:
+            print('None')
+
+    def deleteend(self):
+        if self.head is None:
+            return 'La lista esta vacia'
+        self.head = self.head.next
+
+    def deletestart(self):
+        if self.head is None:
+            return 'la lista esta vacia'
+        if self.head.next is None:
+            self.head = None
+            return
+        temp = self.head
+        while temp.next.next:
+            temp = temp.next
+        temp.next = None
+
+
